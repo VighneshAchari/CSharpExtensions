057bf4968cf708d4efecf7ef2e8a08bb76b07417
VighneshAchari <37047298+VighneshAchari@users.noreply.github.com>
2020-05-24 14:01:37 +0530

    Directory simplified utility methods
    Directory simplified utility methods

---
+++ Source/CSharpExtensions/CSharpExtensions/IO/Dir.cs
@@ -0,0 +1,87 @@
+﻿//Copyright (c) 2020
+//Author: Vighneshwar Achari
+//Notes: Contains simplified utility methods for directory operations
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace System.IO
+{
+    /// <summary>
+    /// Contains simplified utility methods for directory operations
+    /// </summary>
+    public class Dir
+    {
+        /// <summary>
+        /// Creates the directry specified by path if not found.
+        /// </summary>
+        /// <param name="path">Path of the directory to create</param>
+        public static void Create(string path)
+        {
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        /// <summary>
+        /// Creates the directory specified by path if condition satisfies
+        /// </summary>
+        /// <param name="path">Path of the directory to create</param>
+        /// <param name="condition">Condition to check</param>
+        public static void CreateIf(string path, Func<bool> condition)
+        {
+            if(condition == null)
+            {
+                throw new ArgumentNullException("condition");
+            }
+
+            if(condition.Invoke())
+            {
+                Create(path);
+            }
+        }
+
+        /// <summary>
+        /// Renames a directory
+        /// </summary>
+        /// <param name="path">Path of the directory</param>
+        /// <param name="newName">New name of the directory</param>
+        public static void Rename(string path, string newName)
+        {
+            if(string.IsNullOrWhiteSpace(newName))
+            {
+                throw new ArgumentException("Parameter cannot be null or empty.", "newName");
+            }
+
+            var dir = new DirectoryInfo(path);
+
+            if (dir != null)
+            {
+                Directory.Move(dir.FullName, Path.Combine(dir.Parent.FullName, newName));
+            }
+        }
+
+        /// <summary>
+        /// Deletes the directory if the condition specified satisfies.
+        /// </summary>
+        /// <param name="path">Path of the directory</param>
+        /// <param name="condition">Condition to check</param>
+        public static void DeleteIf(string path, Func<bool> condition)
+        {
+            if(condition == null)
+            {
+                throw new ArgumentNullException("condition");
+            }
+
+            var isSatisfied = condition.Invoke();
+
+            if(isSatisfied)
+            {
+                Directory.Delete(path, true);
+            }
+        }
+    }
+}
---
+++ Source/Tests/DirTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class DirTests
+    {
+        [TestMethod]
+        public void Test_Create_New_Success()
+        {
+            //arrange
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test1");
+
+            if(Directory.Exists(path))
+            {
+                Directory.Delete(path);
+            }
+
+            //act
+            Dir.Create(path);
+
+            //assert
+            Assert.IsTrue(Directory.Exists(path));
+
+            Directory.Delete(path);
+        }
+
+        [TestMethod]
+        public void Test_Create_If_Dir_Existed_Success()
+        {
+            //arrange
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test2");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            //act
+            Dir.Create(path);
+
+            //assert
+            Assert.IsTrue(Directory.Exists(path));
+
+            Directory.Delete(path);
+        }
+
+        [TestMethod]
+        public void Test_CreateIf_When_Conditon_Satisfies()
+        {
+            //arrange
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test3");
+
+            //act
+            Dir.CreateIf(path, () => true);
+
+            //assert
+            Assert.IsTrue(Directory.Exists(path));
+
+            Directory.Delete(path);
+        }
+
+        [TestMethod]
+        public void Test_CreateIf_When_Conditon_Fails()
+        {
+            //arrange
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test4");
+
+            //act
+            Dir.CreateIf(path, () => false);
+
+            //assert
+            Assert.IsTrue(!Directory.Exists(path));
+        }
+
+        [TestMethod]
+        public void Test_DeleteIf_When_Condition_Satifies()
+        {
+            //arrange
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test5");
+            Directory.CreateDirectory(path);
+
+            //act
+            Dir.DeleteIf(path, () => true);
+
+            //assert
+            Assert.IsTrue(!Directory.Exists(path));
+        }
+
+        [TestMethod]
+        public void Test_DeleteIf_When_Condition_Fails()
+        {
+            //arrange
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test6");
+            Directory.CreateDirectory(path);
+
+            //act
+            Dir.DeleteIf(path, () => false);
+
+            //assert
+            Assert.IsTrue(Directory.Exists(path));
+
+            Directory.Delete(path);
+        }
+
+        [TestMethod]
+        public void Test_Rename_Success()
+        {
+            //arrange
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test7");
+            Directory.CreateDirectory(path);
+            string newName = "Test7_Renamed";
+            string newPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, newName);
+
+            //act
+            Dir.Rename(path, newName);
+
+            //assert
+            Assert.IsTrue(!Directory.Exists(path));
+            Assert.IsTrue(Directory.Exists(newPath));
+
+            Directory.Delete(newPath);
+        }
+    }
+}
